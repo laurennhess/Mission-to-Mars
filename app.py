2d87d4083114d97ab8012a6121b81f3d9ff720f2
Lauren Hess <94096530+laurennhess@users.noreply.github.com>
2022-04-25 18:21:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scraping
 
-app = Flask(__name__, template_folder='template')
+app = Flask(__name__)
 
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
@@ -15,8 +15,10 @@ def index():
 def scrape():
    mars = mongo.db.mars
    mars_data = scraping.scrape_all()
-   mars.update({}, mars_data, upsert=True)
-   return "Scraping Successful!"
+   print("Mars Data")
+   print(mars_data)
+   mars.update_one({}, {"$set":mars_data}, upsert=True)
+   return redirect("/", code = 302)
 
 if __name__ == "__main__":
     app.run()
